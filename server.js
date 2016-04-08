54194ffe7b414435b3413cb9521760595a5a1cfc
claire <claire.mariko.smith@gmail.com>
2016-04-08 21:41:55 +0100

    connected to server, and flight and hotel data uploaded

--- server.js
+++ server.js
@@ -12,6 +12,48 @@ app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, + 'client/build/index.html'));
 })
 
+app.get('/flights', function(req,res){
+  var url = 'mongodb://localhost:27017/flightInformation';
+  MongoClient.connect(url, function(err, db){
+    if(err){
+      console.log(err);
+      return;
+    }
+    var collection = db.collection('flights');
+    collection.find({}).toArray(function(err, docs){
+      res.json(docs);
+      db.close();
+    })
+  })
+});
+
+
+app.get('/hotels', function(req,res){
+  var url = 'mongodb://localhost:27017/flightInformation';
+  MongoClient.connect(url, function(err, db){
+    if(err){
+      console.log(err);
+      return;
+    }
+    var collection = db.collection('hotels');
+    collection.find({}).toArray(function(err, docs){
+      res.json(docs);
+      db.close();
+    })
+  })
+});
+
+
+
+
+
+
+
+
+
+
+app.use(express.static('client/build'));
+
 var server = app.listen(3000, function() {
   var host = server.address().address;
   var port = server.address().port;
