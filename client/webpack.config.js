24bd94a335c1660893ecd356a7be230627b1e635
rpmonteiro <ric.p.monteiro@gmail.com>
2016-04-08 15:49:31 +0100

    added loaders for the frontend assets

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -35,8 +35,20 @@ config = {
     loaders: [
       {
         test: /\.sass$/,
+        exclude: [/node_modules/],
         loader: ExtractTextPlugin.extract(
           "style-loader", "css-loader!sass-loader")
+      },
+      // fonts and svg
+      { test: /\.woff(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=application/font-woff" },
+      { test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=application/font-woff" },
+      { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=application/octet-stream" },
+      { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: "file" },
+      { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=image/svg+xml" },
+      {
+        // images
+        test: /\.(ico|jpe?g|png|gif)$/,
+        loader: "file"
       },
     ],
   },
