c068fd290622dcb03bab2bf0fc2a52fe3fc6c37e
rpmonteiro <ric.p.monteiro@gmail.com>
2016-04-08 13:53:29 +0100

    sass file finally being compiled

--- client/src/app.js
+++ client/src/app.js
@@ -1,6 +1,7 @@
 var SearchFormView = require('./views/landing_page.js');
 var test = new SearchFormView;
-require('./stylesheets/main.scss');
+module.hot.accept();
+require('css!./stylesheets/main.scss');
 
 window.onload = function() {
   test.sayHi();
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,5 +1,6 @@
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
 var path = require('path');
+var webpack = require('webpack');
 var PATHS = {
   app: path.join(__dirname, 'src/app.js'),
   build: path.join(__dirname, 'build'),
@@ -27,18 +28,25 @@ config = {
   loaders: [
     {
       test: /\.scss$/,
-      include: /src/,
-      loader: "css-loader!sass-loader"
+      loader: "style!css!sass"
     },
+    {
+      test: /\.scss$/,
+      loader: ExtractTextPlugin.extract(
+        "style",
+        "css!sass")
+     },
   ],
-  sassLoader: {
-    includePaths: [path.resolve(__dirname, "./src/styles/")]
-  },
   plugins: [
-    new ExtractTextPlugin('public/style.css', {
-      allChunks: true
-    })
-  ]
+    new webpack.HotModuleReplacementPlugin(),
+    new ExtractTextPlugin('styles.css')
+  ],
+  devServer: {
+    contentBase: ".",
+    inline: true,
+    watch: true,
+    hot: true,
+  }
 }
 
 
