be6f82869163824e3c4448f17babcbb2edfa519b
rpmonteiro <ric.p.monteiro@gmail.com>
2016-04-09 14:05:40 +0100

    filtered the initial results to an array

--- client/src/app.js
+++ client/src/app.js
@@ -1,26 +1,45 @@
-
-    //WHERE IS IT GETTING SENT TO - FLIGHTS API FOR THE MONTH OF MAY
-    var url = 'https://api.sandbox.amadeus.com/v1.2/flights/low-fare-search?apikey=bX8HkNGmgrYd81Z9ne6OyMp4WhAiYoyS&origin=LON&destination=SYD&departure_date=2016-06-25&currency=GBP&number_of_results=3'
-    //MAKE A REQUEST
-    var request = new XMLHttpRequest();
-    //WHERE IS THIS REQUEST GOING
-    request.open("GET", url);
-    console.log("opening the request");
-    //WHAT TO DO WHEN IT COMPLETES
-    request.onload = function () {
-      console.log("loading the request");
-        if (request.status === 200) {
-            var jsonString = request.responseText;
-            var flights = JSON.parse(jsonString);
-            console.log("parsed the request");
-            // main(flightss.results);
-          console.log(flights)
+var _ = require('lodash');
+window.onload = function() {
+  //WHERE IS IT GETTING SENT TO - FLIGHTS API FOR THE MONTH OF MAY
+  var url = 'https://api.sandbox.amadeus.com/v1.2/flights/low-fare-search?apikey=bX8HkNGmgrYd81Z9ne6OyMp4WhAiYoyS&origin=LON&destination=SYD&departure_date=2016-06-25&currency=GBP&number_of_results=5'
+  //MAKE A REQUEST
+  var request = new XMLHttpRequest();
+  //WHERE IS THIS REQUEST GOING
+  request.open("GET", url);
+  console.log("opening the request");
+  //WHAT TO DO WHEN IT COMPLETES
+  request.onload = function () {
+    console.log("loading the request");
+      if (request.status === 200) {
+          var jsonString = request.responseText;
+          var flights = JSON.parse(jsonString);
+          console.log("parsed the request");
+          // main(flights.results);
+        console.log(flights)
+        var results = flights.results;
+        var filteredData = [];
+        var superFilteredData = [];
+        for (var flight of results) {
+          filteredData.push(flight);
         }
+        console.log(filteredData);
+        var importantKeys = [
+        'departs_at',
+        'arrives_at',
+        'total_fare'
+        ]
+        // filteredData.map(function(item) {
+        //   var result = _.pick(item, importantKeys);
+        //   superFilteredData.push(result);
+        //   return result;
+        // })
 
-    }
-    request.send(null);
-};
+        var simonIsCool = _.mapValues(filteredData, 'total_fare');
+        console.log(simonIsCool);
+      }
 
-
+  }
+  request.send(null);
+}
 
 
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -5,8 +5,6 @@ config = {
     path: "./build/",
   },
   devtool: "source-map",
-  loaders: [
-  ]
 }
 
 
