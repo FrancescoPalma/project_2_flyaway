66cab5a11eb63ca9880079bbb7a968b592ba0c8f
rpmonteiro <ric.p.monteiro@gmail.com>
2016-04-12 12:31:39 +0100

    added the missing appjs file

---
+++ client/src/app.js
@@ -0,0 +1,15 @@
+require('./stylesheets/animate.css');
+require('./stylesheets/main.sass');
+require('./stylesheets/style.css');
+require('./stylesheets/skeleton.css');
+var getFlightData = require('./models/flight_api').getFlightData;
+var getOutboundFlights = require('./models/flight_api').getOutboundFlights;
+var getFlightPrice = require('./models/flight_api').getFlightPrice;
+
+window.onload = function() {
+  console.log("I'm loading");
+
+  $(document).ready(function() {
+    getFlightData('SYD', 'LON', '2016-07-28');
+  })
+}
