51ca609ca6975a3a433be02effae8065190a88f7
Francesco <fpfrancescopalma7@gmail.com>
2016-04-13 11:34:22 +0100

    Dates work!!!!

--- client/src/app.js
+++ client/src/app.js
@@ -29,16 +29,28 @@ window.onload = function() {
   datePickerButton.onclick = function(e) {
     e.preventDefault();
     datepicker.show();
+
     datepicker.onSelect = function() {
-      datepicker.range = true;
-      var getDate = this.toLocaleDateString();
-      console.log(getDate);
-      datepicker.onClose = function() {
-        var formatDate = getDate.split('/');
+      var getFirstDate = this.toLocaleDateString();
+      console.log(getFirstDate);
+      var formatDate = getFirstDate.split('/');
+      var day = formatDate[1];
+      var month = formatDate[0];
+      var year = formatDate[2];
+      var formattedDate = year + "-" + month + "-" + day;
+
+      var displayFirstDate = document.getElementById('first-date'); 
+      displayFirstDate.innerHTML = formattedDate; 
+      
+      datepicker.onSelect = function() {
+        var getSecondDate = this.toLocaleDateString();
+        var formatDate = getSecondDate.split('/');
         var day = formatDate[1];
         var month = formatDate[0];
         var year = formatDate[2];
-        console.log(year + "-" + month + "-" + day);
+        var formattedDate = year + "-" + month + "-" + day;
+        var displayDate = document.getElementById('second-date');
+        displayDate.innerHTML = formattedDate;
       }
     }
   }
